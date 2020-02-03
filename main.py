ea4bef56d3e72aaec06e7ac7c53cfef125b8ec12
dilipvaleti <57723484+dilipvaleti@users.noreply.github.com>
2020-02-03 22:36:26 +0530

    Update main.py
    main.py module got completed, it will find the all actionable sentences available in the e-mail data set, and classify the each email under one of Action or Dummy category.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 import re
 import nltk
 
-df = pd.read_csv('emails.csv',nrows=1000)
-print(df.head())
+email_df = pd.read_csv('test.csv')
+
+#print(email_df.head())
 splitter = re.compile('[^.!?]*[\w]\s*[.!?]')
 bow = {}
 sentences = []
@@ -32,3 +33,95 @@ def is_actionable(query):
             if query.count(' ')>2:
                 return True
     return False
+def get_message(colln,idx):
+    _msg = colln.iloc[idx]['message']
+    msgs = _msg.split(':')[-1].split('/n')
+    msgs_join = ' '.join(msgs)
+    msg_sents = splitter.findall(msgs_join)
+    #print('Dilip going to print -->',msg_sents)
+    return msg_sents
+
+
+
+# Saving actionable sentences in this list and then to a file.
+sentences = []
+tp = 0
+fp = 0
+fn = 0
+
+pos_dict = {'MD':'1','VB':'2','VBG':'2','VBN':'2','VBP':'2','VBZ':'2','WP':'3'}
+patterns = ['142','12','32','342','242']
+
+def evaluate(idf):
+    if idf[0]=='2':
+        return True
+
+    for p in patterns:
+        if p in idf:
+            return True
+    return False
+
+for k in tqdm(range(len(email_df))):
+    try:
+        sents_k = get_message(email_df,k)
+        total_count+=len(sents_k)
+        for s in sents_k:
+            # Evaluation by findin patterns in nltk
+            tags = nltk.pos_tag(s.strip().split())
+            pos = [item[1] for item in tags]
+            key_idf = ''
+            for p in pos:
+                if p in pos_dict.keys():
+                    key_idf+=pos_dict[p]
+                else:
+                    key_idf+='4'
+            #print("Dilip key_idf value",key_idf)
+            gt_sent = evaluate(key_idf)
+            #print('Dilip--> gt sent: ',gt_sent)
+            if is_actionable(s.strip()):
+                sentences.append(s.strip())
+                #print('Dlip--< sentences :',sentences)
+                if gt_sent:
+                    tp+=1
+                else:
+                    fp+=1
+            else:
+                if gt_sent:
+                    fn+=1
+                    #print('Dilip tags :',tags)
+        if len(sentences) > LIMIT:
+            save_results = open('save_results.txt','a+')
+            actionable_sentences+=len(sentences)
+            print('Results Saved for ' + str(len(sentences)) + ' lines.')
+            results = '\n'.join(sentences)
+            save_results.write(results)
+            save_results.close()
+            sentences = []
+    except:
+        print('Message Not Retrieved')
+#classify the email int he date set into Action/Dummy        
+def detect(sen):
+    for y in sentences:
+        if y in sen:
+            return "Action"
+        
+    return "Dummy"
+email_df['State']=email_df['message'].apply(lambda x : detect(x))                                        
+email_df.to_csv('email_classify.csv')
+print('\n\n\n\n============== Summary ====================\n')
+precission=(tp)/(tp+fp)
+Recall=(tp)/(tp+fn)
+F1=2*precission*Recall/(precission+Recall)
+print('Precision :', (tp)/(tp+fp))
+print('Recall :', (tp)/(tp+fn))
+print('F1 score : ',F1)
+save_results = open('./outputs/save_results.txt','a+')
+actionable_sentences+=len(sentences)
+print('Results Saved for ' + str(len(sentences)) + ' lines.')
+results = '\n'.join(sentences)
+save_results.write(results)
+save_results.close()
+
+
+print('Number of Actionable Sentences : ', actionable_sentences)
+print('Total Sentences : ', total_count)
